e03d98f90474340f118f4c7bf716fe943bacc08f
Kiana <kiana0718@gmail.com>
2025-10-06 01:01:06 +1100

    feat: cards

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,12 +1,33 @@
-import styles from './home.module.scss'
-import React from 'react';
+import Card from "../../components/Card/Card";
+import Panel from "../../components/Panel/Panel";
+import styles from "./home.module.scss";
+import React from "react";
+
+const cardContent = [
+  {
+    cardName: "Meowster Card",
+    expiry: "Exp 12/46",
+  },
+  {
+    cardName: "Kibble Kard",
+    expiry: "Exp 01/28",
+  },
+];
 
 const Home: React.FC = () => {
-    return(
+  return (
     <div className={styles.home}>
-        <h1>Home</h1>
+      <h1>Balance</h1>
+      {/* TODO: Fetch balance from backend */}
+      <Panel content="You have 1000 cat coins" />
+      <h2>Cards</h2>
+      <div className={styles.cardContainer}>
+        {cardContent.map((panel, index) => (
+          <Card key={index} cardName={panel.cardName} expiry={panel.expiry} />
+        ))}
+      </div>
     </div>
-    );
-}
+  );
+};
 
 export default Home;
